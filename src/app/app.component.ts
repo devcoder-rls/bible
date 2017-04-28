7231d0052b7e972111318bd42f3010d85ff817b7
LBVDC\CirilloR <cirillor@lbv.org.br>
2017-04-28 08:34:25 -0300

    Improve night mode feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { SettingsService } from '../providers/settings-service';
 
+import { SettingsModel }  from '../models/settings-model'
+
 import { HomePage } from '../pages/home/home';
 
 @Component({
@@ -12,13 +14,22 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage = HomePage;
 
+  settings: SettingsModel;
+
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, settingsService: SettingsService) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
 
+      this.settings = new SettingsModel();
+
       settingsService.apply();
+
+      settingsService.getSettings()
+      .then(settings => {
+        this.settings = settings;
+      });      
 
       splashScreen.hide();
     });
