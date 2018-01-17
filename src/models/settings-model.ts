2c9395545834fc093758403b1e325ed994138313
LBVDC\CirilloR <cirillor@lbv.org.br>
2018-01-17 13:35:34 -0300

    Improve UI with multiples fonts

--- src/models/settings-model.ts
+++ src/models/settings-model.ts
@@ -1,16 +1,16 @@
 export class Font {
-	constructor(public name: string, public label: string) {}
+  constructor(public name: string, public label: string) {}
 }
 
 export class SettingsModel {
 
   static readonly AVAILABLE_FONTS: Array<Font> = 
   [
-  	new Font('roboto', 'Roboto (Padrão)'),
-  	new Font('arial', 'Arial'),
-  	new Font('palatino', 'Palatino'),
-  	new Font('times-new-roman', 'Times New Roman'),
-  	new Font('verdana', 'Verdana')
+    new Font('roboto', 'Roboto (Padrão)'),
+    new Font('arial', 'Arial'),
+    new Font('helvetica', 'Helvetica'),
+    new Font('palatino', 'Palatino'),
+    new Font('times-new-roman', 'Times New Roman'),
   ];
 
   constructor(public textsize?: number, public fontname?: string, public showPassageTitle?: boolean, public showBookmarks?: boolean, public keepScreenOn?: boolean, public nightMode?: boolean) {
